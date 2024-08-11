410792ffb391d5a2d1893c748617ed99c387c5a1
thang.dz <phamducthang18cm@gmail.com>
2024-08-11 21:58:39 +0700

    add new code

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -1,23 +1,21 @@
 <template>
-     <main style="min-height: 50vh; margin-top: 2rem;">
+    <main style="min-height: 50vh; margin-top: 2rem;">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-                    <div class="relative">
-                        <input type="text" class="form-control form-control-lg padding-right-lg"
-                            placeholder="+ Add new task. Press enter to save." />
-                    </div>
-                        <Tasks :tasks="uncompletedTasks"/>
+                    <NewTask @added="handleAddedTask" />
+                    <Tasks :tasks="uncompletedTasks" />
 
-                        <div class="text-center my-3" v-show="showToggleCompletedBtn">
-                            <button class="btn btn-sm btn-secondary" 
+                    <div class="text-center my-3" v-show="showToggleCompletedBtn">
+                        <button class="btn btn-sm btn-secondary" 
                             @click="showCompletedTasks = !showCompletedTasks">
-                                <span v-if="!showCompletedTasks">Show Completed</span>
-                                <span v-else>Hide Completed</span>
-                            </button>
-                        </div>
-                        <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks"/>
+                            <span v-if="!showCompletedTasks">Show Completed</span>
+                            <span v-else>Hide Completed</span>
+                        </button>
+                    </div>
+
+                    <Tasks :tasks="completedTasks" v-if="shouldShowCompletedTasks" />
                 </div>
             </div>
         </div>
@@ -25,22 +23,34 @@
 </template>
 
 <script setup>
-    import {computed, onMounted,ref} from "vue";
-    import {allTasks} from "../http/task-api";
-    import Tasks from "../components/tasks/Tasks.vue";
-    const tasks = ref([]);
+import { computed, onMounted, ref } from "vue";
+import { allTasks,createTask } from "../http/task-api";
+import NewTask from "@/components/tasks/NewTask.vue";
+import Tasks from "../components/tasks/Tasks.vue";
 
-    onMounted(async () => {
-        const {data} = await allTasks();
-        tasks.value = data.data;
-        console.log(completedTasksIsVisible.value &&showCompletedTasks.value);
-       
-    })
-    const uncompletedTasks = computed(()=> tasks.value.filter(task =>!task.is_completed))
-    const completedTasks = computed(()=> tasks.value.filter(task =>task.is_completed))
-    const showToggleCompletedBtn =computed(
-        ()=> uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
-    );
-    const completedTasksIsVisible = computed(()=> uncompletedTasks.value.length === 0 || completedTasks.value.length > 0)
-    const showCompletedTasks = ref(false);
+const tasks = ref([]);
+
+onMounted(async () => {
+    const { data } = await allTasks();
+    tasks.value = data.data;
+});
+
+const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed));
+const completedTasks = computed(() => tasks.value.filter(task => task.is_completed));
+
+const showToggleCompletedBtn = computed(() => 
+    uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+);
+
+const shouldShowCompletedTasks = computed(() =>
+    (uncompletedTasks.value.length === 0 || completedTasks.value.length > 0) && showCompletedTasks.value
+);
+
+const showCompletedTasks = ref(false);
+
+const handleAddedTask = async(task) => {
+    const {data : createdTask} = await createTask(task);
+
+    tasks.value.unshift(createdTask.data);
+};
 </script>
