81ec862b641a806b1417b50b7983359ca9e77765
thang18 <phamducthang18cm@gmail.com>
2024-08-14 07:43:22 +0700

    add code 14/8

--- src/components/tasks/Tasks.vue
+++ src/components/tasks/Tasks.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="card mt-2" v-if="tasks.length && show">
         <ul class="list-group list-group-flush">
-            <Task v-for="task in tasks" :task="task" :key="task.id"/>
+            <Task v-for="task in tasks" 
+            :task="task" 
+            :key="task.id" 
+            @updated="$emit('updated',$event)"
+            @completed="$emit('completed',$event)"
+            @removed="$emit('removed',$event)"
+            />
         </ul>
     </div>
 </template>
@@ -19,5 +25,18 @@ defineProps({
         default: true,
     },
 })
+// const emit = defineEmits(['updated','completed']);
+// const handleUpdatedTask = (updatedTask) => {
+//     emit('updated', updatedTask); // Phát sự kiện với dữ liệu task đã cập nhật
+// };
 
+// const handleCompletedTask = (completedTask) => {
+//     emit('completed', completedTask); // Phát sự kiện với dữ liệu task đã hoàn thành
+// };
+// const handleRemovedTask =() => {
+//     if(confirm('Are you sure you want to remove')){
+//         console.log('hehe');
+        
+//     }
+// };
 </script>
